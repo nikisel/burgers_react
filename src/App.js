6e8088b4d4f087528ea775d82a6053963e928019
Natalie <kisel_n_i@mail.ru>
2025-11-17 13:13:32 +0000

    update 17.11.2025

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello Burgers!</h1>
+      <h1>Hello Burgers</h1>
       <h2>Starting editing to see some magic happen!</h2>
     </div>
   );
